dd96fcf2c0ff82edc255ccb248349cfd15c8ae0a
SiddharthaManthurthi <manthurthisiddhartha@gmail.com>
2026-01-28 19:36:50 +0530

    Variables with $

--- 06-variable.sh
+++ 06-variable.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
-START_TIME_STAMP=$(date)
+START_TIME_STAMP=$(date +%s)
 echo " the script is executed at:$START_TIME_STAMP"
 
-sleep 10
-END_TIME_STAMP=$(date)
+sleep 100
+END_TIME_STAMP=$(date +%s)
 TOTAL_TIME=$((END_TIME_STAMP-START_TIME_STAMP)))
 echo " the script is executed in :$TOTAL_TIME seconds"
