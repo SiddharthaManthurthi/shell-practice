b83e95eafbb9da5a37aa9f7e29680fef19bd2941
SiddharthaManthurthi <manthurthisiddhartha@gmail.com>
2026-01-28 23:54:07 +0530

    Variables with $

--- 10-installation.sh
+++ 10-installation.sh
@@ -1,3 +1,11 @@
 #! /bin/bash
+USERID=$(id -u)
 
-dnf install nginx -y
+if [ $USERID -ne 0 ]; then
+  echo " Please run this script with root user access"
+
+  fi
+
+  echo "intalling nginx web server"
+  dnf install nginx -y
+
