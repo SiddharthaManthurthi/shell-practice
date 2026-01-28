1c486dd33dac8405a27a1e4282186a17bc919810
SiddharthaManthurthi <manthurthisiddhartha@gmail.com>
2026-01-28 12:35:34 +0530

    conversation

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Suresh :: Hello Ramesh, How are you?"
+echo "Ramesh :: Hi Suresh, I am fine. How about you?"
+echo "Suresh :: I am doing great. Thanks for asking!"
+echo "Ramesh :: That's good to hear. What are you learning these days?"
+echo "Suresh :: I am learning shell scripting. It's quite interesting!"
