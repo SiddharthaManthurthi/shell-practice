1af0316a290a220d388a7dfc8410b2e12200f0e6
SiddharthaManthurthi <manthurthisiddhartha@gmail.com>
2026-01-28 18:34:08 +0530

    Variables with $

--- 05-variable.sh
+++ 05-variable.sh
@@ -5,4 +5,4 @@ read USER_NAME
 echo "welcome $USER_NAME to the world of shell scripting!"
 echo "Please enter your password:"
 read -s PASSWORD
-echo "PASSWORD IS $USER_NAME"
+echo "Please confirm your password is correct":$PASSWORD
