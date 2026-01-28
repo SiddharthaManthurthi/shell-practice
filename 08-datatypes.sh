5bf48e7664231d047b417744788a33af882491ab
SiddharthaManthurthi <manthurthisiddhartha@gmail.com>
2026-01-28 22:50:14 +0530

    Variables with $

--- 08-datatypes.sh
+++ 08-datatypes.sh
@@ -3,3 +3,9 @@ NUM1=10
 NUM2=20
 SUM=$((NUM1 + NUM2))
 echo the sum is $SUM
+
+## Array means list of items
+FRUITS=("Apple" "Banana" "Orange" "Grapes")
+echo "Fruits List: ${FRUITS[@]}"
+echo "First Fruit: ${FRUITS[0]}"
+echo "Second Fruit: ${FRUITS[1]}"
