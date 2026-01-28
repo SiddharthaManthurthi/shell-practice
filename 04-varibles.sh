f75a5f1859c9295a3e8b7d8443760e8b80be587b
SiddharthaManthurthi <manthurthisiddhartha@gmail.com>
2026-01-28 15:09:12 +0530

    Specialvariable

---
+++ 04-varibles.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PERSON1=$1
+PERSON2=$2
+echo "$PERSON1 :: Hello $PERSON2, How are you?"
+echo "$PERSON2 :: Hi $PERSON1, I am fine. How about you?"
+echo "$PERSON1 :: I am doing great. Thanks for asking!"
+echo "$PERSON2 :: That's good to hear. What are you learning these days?"
+echo "$PERSON1 :: I am learning shell scripting. It's quite interesting!"
