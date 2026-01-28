d9e5fc08dd9c737a14603d75ce206bf66d46a5b6
SiddharthaManthurthi <manthurthisiddhartha@gmail.com>
2026-01-28 23:32:55 +0530

    Variables with $

---
+++ 09-conditions.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+NUMBER=$1
+
+#-gt means greater than
+#-lt means less than
+#-eq means equal to
+# -ne means not equal to
+if [ $NUMBER -gt 20 ]; then
+echo "Given number : $NUMBER is greater than 20"
+fi
