7391cb65a20f08da7a96361ff72fe42b8ee3e90f
SiddharthaManthurthi <manthurthisiddhartha@gmail.com>
2026-01-29 13:11:06 +0530

    if condition test

--- 10-installation.sh
+++ 10-installation.sh
@@ -3,9 +3,10 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
   echo " Please run this script with root user access"
+  exit 1
+  
+fi
 
-  fi
-
-  echo "intalling nginx web server"
-  dnf install nginx -y
+echo "intalling nginx web server"
+dnf install nginx -y
 
