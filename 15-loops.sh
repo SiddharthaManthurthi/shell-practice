67b45a4267c71dd66f5e802c07e320d219b5cbcc
SiddharthaManthurthi <manthurthisiddhartha@gmail.com>
2026-01-31 20:59:09 +0530

    Validate

---
+++ 15-loops.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="/var/log/shell-script/$0.log"
+
+if [ $USERID -ne 0 ]; then
+  echo " Please run this script with root user access" | tee -a $LOGS_FILE
+  exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE () {
+   if [ $1 -ne 0 ]; then
+      echo "$2 installation failed" | tee -a $LOGS_FILE
+      exit 1
+    else
+        echo "$2 installation successful"
+    fi
+}
+
+for package in $@ # sudo sh 14-loops.sh nginx mysql nodejs
+do
+   dnf list installed $package &>> $LOGS_FILE
+   if [ $? -ne 0 ]; then
+     echo "$package not installed, installing now"
+     dnf install $package -y &>> $LOGS_FILE
+     VALIDATE $? "$package Installation"
+   else
+        echo "$package is already installed, skipping"
+    fi
+
+done
