97982a0bec9c90b7bf3eb1eb325023a8c8a710d1
SiddharthaManthurthi <manthurthisiddhartha@gmail.com>
2026-02-03 23:12:24 +0530

    old logs delete

--- 24-while-loop.sh
+++ 24-while-loop.sh
@@ -10,4 +10,4 @@ done
 
 while IFS= read -r line;do
     echo "Line Content: $line"
-done < ./21-script.sh #input which file to read line by line
+done < ./21-script-1.sh #input which file to read line by line
