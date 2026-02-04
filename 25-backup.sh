e7723d2bbb8292136c4982366983f12535c1d5a8
SiddharthaManthurthi <manthurthisiddhartha@gmail.com>
2026-02-05 00:12:29 +0530

    crontab

--- 25-backup.sh
+++ 25-backup.sh
@@ -11,33 +11,35 @@ SOURCE_DIR=$1
 DEST_DIR=$2
 DAYS=${3:-14} # 14 Days is the default value, if the user does not provide any value
 
+log(){
+  echo -e "$(date '+%Y-%m-%d %H:%M:%S') :: | $1" | tee -a $LOGS_FILE
+}
+
 if [ $USERID -ne 0 ]; then
-  echo -e "$R Please run this script with root user access $N"
+  log -e "$R Please run this script with root user access $N"
   exit 1
 fi
 
 mkdir -p $LOGS_FOLDER
 
 USAGE(){
-  echo -e "$R USAGE:: sudo backup <SOURCE_DIR> <DEST_DIR> <DAYS> [default 14 days] $N"
+  log -e "$R USAGE:: sudo backup <SOURCE_DIR> <DEST_DIR> <DAYS> [default 14 days] $N"
   exit 1
 }
 
-log(){
-  echo -e "$(date '+%Y-%m-%d %H:%M:%S') :: | $1" | tee -a $LOGS_FILE
-}
+
 
 if [ $# -lt 2 ]; then
   USAGE
 fi
 
 if [ ! -d $SOURCE_DIR ]; then
-  echo -e "$R Source Directory $SOURCE_DIR does not exist $N"
+  log -e "$R Source Directory $SOURCE_DIR does not exist $N"
   exit 1
 fi
 
 if [ ! -d $DEST_DIR ]; then
-  echo -e "$R Destination Directory $DEST_DIR does not exist $N"
+  log -e "$R Destination Directory $DEST_DIR does not exist $N"
   exit 1
 fi
 
@@ -57,18 +59,18 @@ else
   log "File found to archive :$FILES"
   TIMESTAMP=$(date +%F-%H-%M-%S)
   ZIP_FILE_NAME="$DEST_DIR/app-logs-$TIMESTAMP.tar.gz"
-  echo "archive name: $ZIP_FILE_NAME"
+  log "archive name: $ZIP_FILE_NAME"
   tar -zcvf $ZIP_FILE_NAME $(find $SOURCE_DIR -name "*.log" -type f -mtime +$DAYS)
-  
+
   #CHECK ARCHIVE IS SUCCESS OR NOT
   if [ -f $ZIP_FILE_NAME ]; then
     log "Archeival is ... $G SUCCESS $N"
 
     while IFS= read -r filepath; do
     #process each line
-    echo "Deleting file $filepath"
+    log "Deleting file $filepath"
     rm -f "$filepath"
-    echo "deleted file: $filepath"
+    log "deleted file: $filepath"
     done <<< $FILES
   else
     log " Archeival is ....$R FAILURE $N"
