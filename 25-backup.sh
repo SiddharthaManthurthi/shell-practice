b7e91538f1b0c19955bccbf1396a2136c335c6b4
SiddharthaManthurthi <manthurthisiddhartha@gmail.com>
2026-02-04 22:46:11 +0530

    crontab

--- 25-backup.sh
+++ 25-backup.sh
@@ -7,6 +7,9 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+SOURCE_DIR=$1
+DEST_DIR=$2
+DAYS=${3:-14} # 14 Days is the default value, if the user does not provide any value
 
 if [ $USERID -ne 0 ]; then
   echo -e "$R Please run this script with root user access $N"
@@ -22,4 +25,14 @@ USAGE(){
 
 if [ $# -lt 2 ]; then
    USAGE
+fi
+
+if [ ! -d $SOURCE_DIR ]; then
+   echo -e "$R $SOURCE_DIR does not exist $N"
+   exit 1
+fi
+
+if [ ! -d $DEST_DIR ]; then
+   echo -e "$R $DEST_DIR does not exist $N"
+   exit 1
 fi
