35bd17c1db5c883b081964509e8b4d1fef2627cc
SiddharthaManthurthi <manthurthisiddhartha@gmail.com>
2026-02-05 11:45:56 +0530

    crontab

--- 26-disk-usage.sh
+++ 26-disk-usage.sh
@@ -15,15 +15,15 @@ DISK_USAGE=$(df -hT | grep -v Filesystem)
 USAGE_THRESHOLD=3
 
 while IFS= read -r line
-do
- USAGE=$($line |awk '{print $6}' | cut -d "%" -f1)
- PARTITION=$(df -hT | grep -v Filesystem |awk '{print $7}' | cut -d "%" -f1)
 
-  if  ["$USAGE" -ge "$USAGE_THRESHOLD"] then
-    MESSAGE+="High Disk usage on $PARTITION:$USAGE"
-  
+do
+  USAGE=$(echo $line |awk '{print $6}' | cut -d "%" -f1)
+ PARTITION=$(echo $line |awk '{print $7}' | cut -d "%" -f1)
+
+ if  ["$USAGE" -ge "$USAGE_THRESHOLD"] then
+   MESSAGE+="High Disk usage on $PARTITION:$USAGE"
+        
   fi
 
 done <<< $DISK_USAGE
-
 echo "$MESSAGE"
