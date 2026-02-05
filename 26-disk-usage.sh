039adb5aff2b8fcc6c34a5a29454658eddb6d03b
SiddharthaManthurthi <manthurthisiddhartha@gmail.com>
2026-02-05 14:39:12 +0530

    crontab

--- 26-disk-usage.sh
+++ 26-disk-usage.sh
@@ -1,12 +1,11 @@
 #! /bin/bash
 
-
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 MESSAGE=""
-IP_ADDRESS=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/local-ipv4)
+IP_ADDRESS=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
 
 log(){
   echo -e "$(date '+%Y-%m-%d %H:%M:%S') :: | $1" | tee -a $LOGS_FILE
